ce7835a0b3994aae0ff42da13f787aed3e218b9d
David Hope <david.hope@ec.gc.ca>
2022-06-20 12:13:54 -0400

    Add 2nd mask off date

--- WWFun.R
+++ WWFun.R
@@ -18,6 +18,8 @@ ggplot(d |>
   xintercept = lubridate::ymd("2022-03-21")) +
   geom_vline(linetype =2,
   xintercept = lubridate::ymd("2022-04-13")) +
+  geom_vline(linetype =2,
+  xintercept = lubridate::ymd("2022-05-30")) +
   labs(x = "Date", y = "Normalized viral copies") +
   geom_hline(yintercept = tail(d$rollavg, n = 1), colour = 'red')
     # geom_hline(yintercept = 0.0018745)#0.00229)
