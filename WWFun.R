181b42d5587e5e490e86bf14bcc27b9fd7af1a7e
Hope <David.Hope@ec.gc.ca>
2022-10-27 14:09:37 -0400

    Added Gatineau data
    From: https://www.inspq.qc.ca/covid-19/donnees/eaux-usees

--- WWFun.R
+++ WWFun.R
@@ -5,6 +5,11 @@ d <- read_csv("Wastewater/Ottawa/Data/wastewater_virus.csv") |>
   mutate(pavg = mean(c_across(c(covN1_nPMMoV_meanNr,
                       covN2_nPMMoV_meanNr)), na.rm=T )) |> ungroup() |> 
   mutate(rollavg = roll_meanr(pavg, n=7, fill = NA, align = 'right') )
+
+gatineau <- read_csv(here::here("Wastewater/graph_1-1_gatineau.csv")) |> 
+  janitor::clean_names() |> 
+  mutate(sampleDate = lubridate::ymd(date_du_prelevement),
+         rollavg = station_depuration_de_gatineau_moy_7_jours/1e7)
 # m <- read_csv("Wastewater/Ottawa/Data/wwMeasure.csv")
 
 
@@ -25,9 +30,12 @@ ggplot(d |>
        aes(sampleDate, rollavg  )) +
   geom_point(aes(y = pavg),alpha = 0.2)+
   geom_line() +
+  geom_line(data = gatineau |> 
+              filter(sampleDate> lubridate::ymd("2021-12-01")),
+            linetype = 2) +
   ggthemes::theme_clean()+
   geom_vline(data = dates_of_import,
-             linetype =2,
+             linetype =3,
              aes(xintercept = date)) +
   labs(x = "Date", y = "Normalized viral copies") +
   geom_hline(yintercept = tail(d$rollavg, n = 1), colour = 'red')  +
@@ -42,6 +50,7 @@ last_30 <-
          aes(sampleDate, rollavg  )) +
   geom_point(aes(y = pavg),alpha = 0.2)+
   geom_line() +
+  geom_line(data = slice_tail(gatineau, n=30), linetype=2) +
   ggthemes::theme_clean()+
   geom_vline(linetype =2,
              xintercept = lubridate::ymd("2022-03-21")) +
